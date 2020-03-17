276f1d27b7f4cbe752b5b70ff81ba4fc1b18d1ce
harshiBalaga <harshi.b27@gmail.com>
2020-03-17 11:14:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+"use strict"
+
+const data = require("./testData.js").data
+
+class Graph {
+  constructor(props) {
+    this.neighbors = {}
+  }
+
+  addEdge(u, v) {
+    if (!this.neighbors[u]) this.neighbors[u] = []
+    this.neighbors[u].push(v)
+    if (!this.neighbors[v]) this.neighbors[v] = []
+    this.neighbors[v].push(u)
+  }
+
+  shortestPath(start, end) {
+    if (start == end) {
+      return [start, end]
+    }
+
+    var queue = [start],
+        visited = {},
+        predecessor = {},
+        tail = 0,
+        path
+
+    while(tail < queue.length) {
+      var u = queue[tail++]
+      if (!this.neighbors[u]) {
+        continue
+      }
+
+      var neighbors = this.neighbors[u]
+      for(var i = 0; i < neighbors.length; ++i) {
+        var v = neighbors[i]
+        if (visited[v]) {
+          continue
+        }
+        visited[v] = true
+        if (v === end) {   // Check if the path is complete.
+          path = [ v ]   // If so, backtrack through the path.
+          while (u !== start) {
+            path.push(u)
+            u = predecessor[u]
+          }
+          path.push(u)
+          path.reverse()
+          return path
+        }
+        predecessor[v] = u
+        queue.push(v)
+      }
+    }
+
+    return path
+  }
+}
+
+var createGraph = function() {
+  var g = new Graph()
+  g.addEdge(0, 1);
+  g.addEdge(1, 2);
+  g.addEdge(1, 13);
+  g.addEdge(2, 3);
+  g.addEdge(3, 4);
+  g.addEdge(4, 5);
+  g.addEdge(5, 6);
+  g.addEdge(6, 7);
+  g.addEdge(7, 8);
+  g.addEdge(8, 9);
+  g.addEdge(8, 14);
+  g.addEdge(9, 10);
+  g.addEdge(10, 11);
+  g.addEdge(11, 12);
+  g.addEdge(12, 13);
+  g.addEdge(13, 16);
+  g.addEdge(16, 15);
+  g.addEdge(15, 14);
+  return g
+}
+
+var paths = {}
+var shortestPathGraph = createGraph()
+for (var i = 1; i < 17; i++) {
+  paths[i] = shortestPathGraph.shortestPath(0, i)
+}
+
+data.forEach(function(family) {
+  family.path = paths[family.caravan]
+  family.time = paths[family.caravan].length
+})
+data.sort(function (a, b) {
+  return a.time - b.time
+})
+console.dir(data)
+
---
+++ testData.js
@@ -0,0 +1,82 @@
+exports.data = [
+  {
+    familyid: 1,
+    groupsize: 6,
+    caravan: 5
+  },
+  {
+    familyid: 2,
+    groupsize: 4,
+    caravan: 12
+  },
+  {
+    familyid: 3,
+    groupsize: 3,
+    caravan: 9
+  },
+  {
+    familyid: 4,
+    groupsize: 8,
+    caravan: 15
+  },
+  {
+    familyid: 5,
+    groupsize: 2,
+    caravan: 10
+  },
+  {
+    familyid: 6,
+    groupsize: 5,
+    caravan: 3
+  },
+  {
+    familyid: 7,
+    groupsize: 4,
+    caravan: 1
+  },
+  {
+    familyid: 8,
+    groupsize: 2,
+    caravan: 3
+  },
+  {
+    familyid: 9,
+    groupsize: 3,
+    caravan: 7
+  },
+  {
+    familyid: 10,
+    groupsize: 2,
+    caravan: 6
+  },
+  {
+    familyid: 11,
+    groupsize: 4,
+    caravan: 14
+  },
+  {
+    familyid: 12,
+    groupsize: 6,
+    caravan: 11
+  },
+  {
+    familyid: 13,
+    groupsize: 8,
+    caravan: 13
+  },
+  {
+    familyid: 14,
+    groupsize: 3,
+    caravan: 8
+  },
+  {
+    familyid: 15,
+    groupsize: 3,
+    caravan: 16
+  },
+  {
+    familyid: 16,
+    groupsize: 6,
+    caravan: 2
+  }
+];
